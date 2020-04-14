aac6556827dad6779258023f70f53dac01f47cb1
Karin <55209058+kodkarin@users.noreply.github.com>
2020-04-14 11:53:35 +0200

    Setup commmit

--- app/src/main/java/com/example/rockpaperscissorsapp/GameWindow.java
+++ app/src/main/java/com/example/rockpaperscissorsapp/GameWindow.java
@@ -1,5 +1,6 @@
 package com.example.rockpaperscissorsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -11,16 +12,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GameWindow extends AppCompatActivity {
 
     public static final String RESULT_MESSAGE = "com.example.rockpaperscissorsapp.RESULTS";
     public static final String WINNER_MESSAGE = "com.example.rockpaperscissorsapp.WINNER";
+    private final String MOVES_PLAYER1 = "Moves player 1";
+    private final String MOVES_PLAYER2 = "Moves player 2";
+    private final String ROUND_WINNERS = "Winners";
 
     private TextView lastView1 = null;
     private TextView lastView2 = null;
     private TextView resultTextView = null;
+    private List<TextView> movesPlayer1;
+    private List<TextView> movesPlayer2;
+    private ArrayList<Integer> winners;
 
     private Game game = null;
     private int round = 1;
@@ -48,6 +57,10 @@ public class GameWindow extends AppCompatActivity {
         lastView1 = player1TextView;
         lastView2 = player2TextView;
 
+        movesPlayer1 = new ArrayList<>();
+        movesPlayer2 = new ArrayList<>();
+        winners = new ArrayList<>();
+
     }
 
     public void makeMove (View view) {
@@ -74,6 +87,7 @@ public class GameWindow extends AppCompatActivity {
         textView.setId(View.generateViewId());
         textView.setText(text);
 
+        movesPlayer1.add(textView);
         cl.addView(textView);
 
         int id = textView.getId();
@@ -98,10 +112,14 @@ public class GameWindow extends AppCompatActivity {
             game.increaseScorePlayer1();
             lastView1.setTextColor(Color.rgb(54, 108, 76));
             lastView1.setTypeface(null, Typeface.BOLD);
+            winners.add(1);
         } else if (roundWinner == Game.PLAYER2_WINS) {
             game.increaseScorePlayer2();
             lastView2.setTextColor(Color.rgb(54, 108, 76));
             lastView2.setTypeface(null, Typeface.BOLD);
+            winners.add(2);
+        } else {
+            winners.add(0);
         }
 
         round++;
@@ -143,6 +161,7 @@ public class GameWindow extends AppCompatActivity {
         textView.setId(View.generateViewId());
         textView.setText(text);
 
+        movesPlayer2.add(textView);
         cl.addView(textView);
 
         int id = textView.getId();
@@ -165,6 +184,96 @@ public class GameWindow extends AppCompatActivity {
         intent.putExtra(WINNER_MESSAGE, isWinner);
         intent.putExtra(MainActivity.USERNAME_MESSAGE, game.getPlayer1().getUserName());
         startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        String[] movesPlayer1Strings = new String[movesPlayer1.size()];
+        String[] movesPlayer2Strings = new String[movesPlayer2.size()];
+
+        for (int i = 0; i < movesPlayer1.size(); i++) {
+            movesPlayer1Strings[i] = movesPlayer1.get(i).getText().toString();
+        }
+        for (int i = 0; i < movesPlayer2.size(); i++) {
+            movesPlayer2Strings[i] = movesPlayer2.get(i).getText().toString();
+        }
+
+        outState.putStringArray(MOVES_PLAYER1, movesPlayer1Strings);
+        outState.putStringArray(MOVES_PLAYER2, movesPlayer2Strings);
+        outState.putIntegerArrayList(ROUND_WINNERS, winners);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        ConstraintLayout cl = findViewById(R.id.currentGameConstraintLayout);
+        setContentView(cl);
+
+        String[] movesPlayer1Strings = savedInstanceState.getStringArray(MOVES_PLAYER1);
+        String[] movesPlayer2Strings = savedInstanceState.getStringArray(MOVES_PLAYER2);
+        winners = savedInstanceState.getIntegerArrayList(ROUND_WINNERS);
+
+        if (movesPlayer1Strings != null) {
+            for (int i = 0; i < movesPlayer1Strings.length; i++) {
+                TextView textView = new TextView(this);
+                textView.setId(View.generateViewId());
+                textView.setText(movesPlayer1Strings[i]);
+                if (winners.get(i) == Game.PLAYER1_WINS) {
+                    game.increaseScorePlayer1();
+                    textView.setTextColor(Color.rgb(54, 108, 76));
+                    textView.setTypeface(null, Typeface.BOLD);
+                }
+
+                movesPlayer1.add(textView);
+                cl.addView(textView);
+
+                int id = textView.getId();
+
+                ConstraintSet cs = new ConstraintSet();
+                cs.clone(cl);
+
+                cs.connect(id, ConstraintSet.TOP, lastView1.getId(), ConstraintSet.BOTTOM, 10);
+                cs.connect(id, ConstraintSet.START, lastView1.getId(), ConstraintSet.START, ConstraintSet.MATCH_CONSTRAINT);
+
+                cs.applyTo(cl);
+
+                lastView1 = textView;
+            }
+        }
+
+        if (movesPlayer2Strings != null) {
+            for (int i = 0; i < movesPlayer2Strings.length; i++) {
+                TextView textView = new TextView(this);
+                textView.setId(View.generateViewId());
+                textView.setText(movesPlayer2Strings[i]);
+                if (winners.get(i) == Game.PLAYER2_WINS) {
+                    game.increaseScorePlayer2();
+                    textView.setTextColor(Color.rgb(54, 108, 76));
+                    textView.setTypeface(null, Typeface.BOLD);
+                }
+
+                movesPlayer2.add(textView);
+                cl.addView(textView);
+
+                int id = textView.getId();
+
+                ConstraintSet cs = new ConstraintSet();
+                cs.clone(cl);
+
+                cs.connect(id, ConstraintSet.TOP, lastView2.getId(), ConstraintSet.BOTTOM, 10);
+                cs.connect(id, ConstraintSet.START, lastView2.getId(), ConstraintSet.START, ConstraintSet.MATCH_CONSTRAINT);
+
+                cs.applyTo(cl);
+
+                lastView2 = textView;
+            }
+        }
+
+        String resultText = game.getScorePlayer1() + " - " + game.getScorePlayer2();
+        resultTextView.setText(resultText);
     }
 }
 
--- app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rockpaperscissorsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,21 +12,41 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public static final String USERNAME_MESSAGE = "com.example.rockpaperscissorsapp.USERNAME";
+    private final String USERNAME_INPUT = "Username input";
+
+    private EditText usernameInput;
+    private String usernameInputString = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.usernameTextView);
     }
+
+
 
     public void goToGame(View view) {
 
-        EditText usernameInput = findViewById(R.id.usernameTextView);
-        String username = usernameInput.getText().toString();
+        usernameInputString = usernameInput.getText().toString();
 
         Intent intent = new Intent(this, GameWindow.class);
-        intent.putExtra(USERNAME_MESSAGE, username);
+        intent.putExtra(USERNAME_MESSAGE, usernameInputString);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        usernameInputString = usernameInput.getText().toString();
+        outState.putString(USERNAME_INPUT, usernameInputString);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        usernameInputString = savedInstanceState.getString(USERNAME_INPUT);
+        usernameInput.setText(usernameInputString);
     }
 }
